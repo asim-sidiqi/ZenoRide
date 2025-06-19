a618662c391aab77378a8c7a40723d3dccdb0cba
Asim Siddiqui <29aseem.sid@gmail.com>
2025-06-19 14:29:37 +0530

    map-created

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const userRoutes = require('./routes/user.routes')
 const captainRoutes = require('./routes/captain.routes')
+const mapsRoutes = require('./routes/maps.routes')
 dotenv.config();
 
 const connectToDb = require('./db/db')
@@ -21,5 +22,7 @@ app.get('/',(req,res)=>{
 
 app.use('/users', userRoutes)
 app.use('/captains', captainRoutes)
+app.use('/maps', mapsRoutes)
 
 module.exports = app;
+
